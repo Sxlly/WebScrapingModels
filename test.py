ec031f880917c696369fda3823510e07b9cc0046
Sxlly <shaesullivan.buisness1@gmail.com>
2024-06-08 13:16:17 +0800

    prettify

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as scrape_file:
     content = scrape_file.read()
+    print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup.prettify())
