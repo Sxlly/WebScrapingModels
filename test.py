97c0d40f2b30eff740214d3b6f4e71c6d1ed8fd1
Sxlly <shaesullivan.buisness1@gmail.com>
2024-06-11 13:44:33 +0800

    scraping row from table

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -9,6 +10,15 @@ options = Options()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get("https://logs.tf")
+driver.maximize_window()
+
+allLogsTable = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[3]/table/tbody')
+
+allLogsNames = []
+
+for log in allLogsTable.find_elements(By.XPATH, '//tr'):
+
+    row = [item.tezt for item in log.find_elements(By.XPATH, './/td')]
 
 
 
