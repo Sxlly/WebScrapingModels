ebb3602319a0df6b050350b61ab9abddd90fbfc4
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-18 13:04:10 +0800

    scraping just names to list

--- test.py
+++ test.py
@@ -14,17 +14,27 @@ driver.maximize_window()
 
 allLogsTable = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[3]/table/tbody')
 
+allLogsList = []
 allLogsNames = []
 
 for log in allLogsTable.find_elements(By.XPATH, '//tr'):
 
     row = [item.text for item in log.find_elements(By.XPATH, './/td')]
-    allLogsNames.append(row)
+    allLogsList.append(row)
 
-for log in allLogsNames:
-    for detail in log:
-        print("\n")
-        print(detail)
+    for index in range(len(allLogsList)):
+        if allLogsList[index]:
+            allLogsNames.append(allLogsList[index][0])
+    
+
+for logName in allLogsNames:
+    print("\n")
+    print(logName)
+
+
+
+
+
 
 
 
